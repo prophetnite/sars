33d3ad38da06d7caae5f98de572a559e567e5d96
prophetnite <prophetnite@gmail.com>
2015-09-13 19:16:10 -0400

    API updates Core Authentication added

--- main.js
+++ main.js
@@ -1,24 +1,47 @@
 // includes
+// =======================
+// get the packages we need ============
+// =======================
 var express 	=	require('express');
 var path 		=	require('path');
-var bodyParser 	=	require('body-parser');
+var bodyParser  = 	require('body-parser');
+var morgan      = 	require('morgan');
+var mongoose    = 	require('mongoose');
+var config 		= 	require('./config'); 			// get our config file
+var app         =	express();
 
-var app = express();
 
-// configure app
+
+
+// =======================
+// configuration =========
+// =======================
+var port = process.env.PORT || 1337; // used to create, sign, and verify tokens
+mongoose.connect(config.database); // connect to database
+app.set('superSecret', config.secret); // secret variable
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+
+
+// =======================
 // use middleware
-app.use(bodyParser());
+// =======================
+//app.use(bodyParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.use(express.static(path.join(__dirname, 'bower_components')));
 app.use('/assets', express.static('assets'));
 app.use('/assets2', express.static('assets2'));
+app.use(morgan('dev'));
+
 
 // define routes
 app.use(require('./router'))
 
 // start the server
-app.listen(1337, function() { 
-	console.log('Ready on port 1337');
+app.listen(port, function() { 
+	console.log('Ready on port ' + port);
 });
--- router.js
+++ router.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var moment = require('moment');
-var fs = require("fs");
-			  
-var router = express.Router();
 
+// --------------------- INCLUDES AND CONFIGURATION  ---------------------
+var express =	require('express');
+var moment 	=	require('moment');
+var fs 		=	require("fs");
+var User   	=	require('./models/user');
+var jwt    	=	require('jsonwebtoken'); 		// used to create, sign, and verify tokens
+var router 	=	express.Router();
+var config 	= 	require('./config'); 			// get our config file
+
+
+
+
+
+
+// --------------------- TEST DATA  ---------------------
 var userDetails = [{ 
 		namefirst: "Lord",
 		namelast: "_ASDF",
@@ -23,7 +33,14 @@ var ipadd = {
         "accept-language":"en-US,en;q=0.5",
         "accept-encoding":"gzip, deflate",
         "connection":"keep-alive"}
+// --------------------- END TEST DATA  ---------------------
 
+
+
+
+
+
+// --------------------- JSON DATABASE FUNCTIONS  ---------------------
 function writedatabase(data, db_type){
 	fs.writeFile( __dirname + "/" + "iplog_db_" + db_type + ".json", JSON.stringify(data), function (err) {		
 			if (err) return console.log(err);											// ERROR CONSOLE LOGGING
@@ -39,63 +56,40 @@ function loaddatabase(db_type) {
 	}
 	return JSON.parse(database);
 }
+// --------------------- END JSON DATABASE FUNCTIONS  ---------------------
 
-// --------------------- API TEST SYSTEM  ---------------------
-router.get('/api/version' || '/api', function (req, res) {
-       res.end( 'API v. .01 Alpha' );
+
+
+
+
+
+// --------------------- API DATABASE SETUP ROUTES  ---------------------
+// ------- ***** CONFIG CONTAINS LIVE DATABASE PASSWORDS ***** ----------
+// ---------------------------config.secret------------------------------
+// ----------------------------------------------------------------------
+router.get('/api/setup', function (req, res) {
+	// create a sample user
+	var nick = new User({ 
+    	name: 'prophetnite', 
+    	password: 'password',
+    	admin: true 
+	});
+
+	// save the sample user
+	nick.save(function(err) {
+		if (err) throw err;
+		console.log('User saved successfully');
+    	res.json({ success: true });
+	});
 });
+// --------------------- API DATABASE SETUP ROUTES  ---------------------
 
-router.get('/api/create', function (req, res) {	
-	var loggedAddresses = loaddatabase('ip');  											// Loading database function
-	var trackip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;		// Load tracking data to be logged 
-	var ts_hms = moment().format('hh:mm:ss');
-	var ts_ymd = moment().format('L');
-	var fullheader = req.headers;
 
-	loggedAddresses[Object.keys(loggedAddresses).length + 1] = {						// Update in memory data
-		"time": ts_hms,
-		"date": ts_ymd,
-		"remoteAddress": trackip,
-		"headers": fullheader	
-	};
-	console.log(loggedAddresses);														// DEBUG CONSOLE LOGGING
-	writedatabase(loggedAddresses, 'ip');
 
-	res.end('NSA tracking Database: Thanks for reporting in!');
-	//res.redirect('/log_ip');
-
-})
-router.get('/api/read', function (req, res) {
-	var loggedAddresses = loaddatabase('ip');
-	res.end(JSON.stringify(loggedAddresses));
-})
-router.get('/api/read/:id', function (req, res) {
-	// First read existing users.
-	id = loaddatabase('ip');
-	var id = id[req.params.id] 
-	console.log( id );														// DEBUG CONSOLE LOGGING
-	res.end( JSON.stringify(id));
-   
-})
-router.get('/api/update', function (req, res) {
-	var loggedAddresses = loaddatabase('ip');
-	res.end(JSON.stringify('NOT YET IMPLEMENTED'));
-})
-
-router.get('/api/delete/:id', function (req, res) {
-	var loggedAddresses = loaddatabase('ip');
-		console.log('total Length: ' + Object.keys(loggedAddresses).length);		// DEBUG CONSOLE LOGGING
-	delete loggedAddresses[req.params.id];
-    	console.log('after length: ' + Object.keys(loggedAddresses).length );		// DEBUG CONSOLE LOGGING
-    write = writedatabase(loggedAddresses, 'ip');
-	res.end(write);
-})
-// --------------------- END API TEST SYSTEM  ---------------------
 
 
 
 // --------------------- CORE SITE PAGES ---------------------
-
 router.get('/', function (req, res){
 	var iplog_db = loaddatabase('ip');
 	//load data from DB here
@@ -148,34 +142,139 @@ router.get('/about', function (req, res){
 		user: userDetails
 	});
 });
-
 //  --------------- END CORE SITE PAGES -------------------
-router.post('/add', function (req, res) {
-	//var newItem = req.body.newItem;
 
-	var trackip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-	ts_hms = moment().format('hh:mm:ss');
-	ts_ymd = moment().format('L');
-	
-	loggedAddresses.push({
-		id: loggedAddresses.length + 1,
-		time: ts_hms,
-		date: ts_ymd,
-		ip: trackip	
+
+
+
+
+
+// --------------------- API (MONGO) AUTHENTICATION ROUTES  ---------------------
+// -- Everything after token verify requires authentication
+router.post('/api/authenticate', function (req, res) {
+  // find the user
+	User.findOne({
+		name: req.body.name
+	}, function(err, user) {
+    	if (err) throw err;
+	    if (!user) {
+   			res.json({ success: false, message: 'Authentication failed. User not found.' });
+    	} else if (user) {
+      	// check if password matches
+      	if (user.password != req.body.password) {
+        	res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+      	} else {
+        // if user is found and password is right
+        // create a token
+        var token = jwt.sign(user, config.secret, {
+          expiresInMinutes: 1440 // expires in 24 hours
+        });
+        // return the information including token as JSON
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }   
+    }
+  });
+});
+
+// route to show version message (GET http://localhost:8080/api/)
+router.get('/api', function (req, res) {
+       res.json({ message: 'API v.01: use http://server:port/api/authenticate to receive acess token' });
+});
+
+// route middleware to verify a token
+router.use(function (req, res, next) {
+	// check header or url parameters or post parameters for token
+  	var token = req.body.token || req.query.token || req.headers['x-access-token'];
+	// decode token
+  	if (token) {
+    // verifies secret and checks exp
+    jwt.verify(token, config.secret	, function(err, decoded) {      // should be set to app.get('globalvar')
+    	if (err) {
+        	return res.json({ success: false, message: 'Failed to authenticate token.' });    
+    	} else {
+        	// if everything is good, save to request for use in other routes
+        	req.decoded = decoded;    
+        	next();
+      	}	
+    });
+  	} else {
+	    // if there is no token return an error
+	    return res.status(403).send({ 
+	        success: false, 
+	        message: 'No token provided.' 
+	    });
+	}
+});
+
+// route to return all users (GET http://localhost:8080/api/users)
+router.get('/api/users', function (req, res) {
+	User.find({}, function (err, users) {
+		res.json(users);
 	});
+});   
+// --------------------- END API (MONGO) AUTHENTICATION ROUTES  ---------------------
 
-	res.redirect('/log_ip');
-});
 
-router.post('/delete', function (req, res) {
-	var delItem = req.body.delItem;
 
-	console.log('ITEM TO DELETE ' + delItem);   // CONSOLE
-	console.log(loggedAddresses[delItem]);	    // CONSOLE
-	loggedAddresses.splice(delItem, 1);
-	console.log(loggedAddresses[delItem]);		// CONSOLE
-	
-	res.redirect('/log_ip');
-});
+
+
+
+// --------------------- API (JSON-TEXT) TEST SYSTEM  ---------------------
+router.get('/api/create', function (req, res) {	
+	var loggedAddresses = loaddatabase('ip');  											// Loading database function
+	var trackip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;		// Load tracking data to be logged 
+	var ts_hms = moment().format('hh:mm:ss');
+	var ts_ymd = moment().format('L');
+	var fullheader = req.headers;
+
+	loggedAddresses[Object.keys(loggedAddresses).length + 1] = {						// Update in memory data
+		"time": ts_hms,
+		"date": ts_ymd,
+		"remoteAddress": trackip,
+		"headers": fullheader	
+	};
+	console.log(loggedAddresses);														// DEBUG CONSOLE LOGGING
+	writedatabase(loggedAddresses, 'ip');
+
+	res.end('NSA tracking Database: Thanks for reporting in!');
+	//res.redirect('/log_ip');
+
+})
+
+router.get('/api/read', function (req, res) {
+	var loggedAddresses = loaddatabase('ip');
+		for(var attributename in loggedAddresses){
+    		console.log(attributename+": "+loggedAddresses[attributename]);
+		}	
+	res.end(JSON.stringify(loggedAddresses));
+})
+
+router.get('/api/read/:id', function (req, res) {
+	// First read existing users.
+	id = loaddatabase('ip');
+	var id = id[req.params.id] 
+	console.log( id );														// DEBUG CONSOLE LOGGING
+	res.end( JSON.stringify(id));
+   
+})
+
+router.get('/api/update', function (req, res) {
+	var loggedAddresses = loaddatabase('ip');
+	res.end(JSON.stringify('NOT YET IMPLEMENTED'));
+})
+
+router.get('/api/delete/:id', function (req, res) {
+	var loggedAddresses = loaddatabase('ip');
+		console.log('total Length: ' + Object.keys(loggedAddresses).length);		// DEBUG CONSOLE LOGGING
+	delete loggedAddresses[req.params.id];
+    	console.log('after length: ' + Object.keys(loggedAddresses).length );		// DEBUG CONSOLE LOGGING
+    write = writedatabase(loggedAddresses, 'ip');
+	res.end(write);
+})
+// --------------------- END API (JSON-TEXT) TEST SYSTEM  ---------------------
 
 module.exports = router;
