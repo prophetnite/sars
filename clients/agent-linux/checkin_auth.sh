f0e8d495c53c06acf664e480273cf99ce3598a9a
prophetnite <prophetnite@gmail.com>
2015-10-10 13:14:00 -0400

    asdf:wq

--- clients/agent-linux/checkin_auth.sh
+++ clients/agent-linux/checkin_auth.sh
@@ -6,6 +6,11 @@ wifi_list="$(grep ESSID wifidump.txt)"
 wifi_list="$(echo $wifi_list | tr -d '\n')"
 wifi_list="$(echo $wifi_list | tr -s ' ' ',')"
 
+authtoken="$(tail -n 1 checkin_auth.sh)"
 
-curl --data "account=8976234&wifiap_list=$wifi_list" -X POST http://newenglandsecure.com/checkin.php
-
+curl --data "account=$authtoken&wifiap_list=$wifi_list" -X POST http://newenglandsecure.com/checkin.php
+echo $authtoken
+exit;
+#
+### user token ###
+asdfasdfasdfasdf
