b07fd8d9b48a22e76d0656391c7f9cd6f7789974
prophetnite <prophetnite@gmail.com>
2015-10-10 12:20:10 -0400

    added clients folder

---
+++ clients/agent-linux/report.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#iwlist wlan0 scanning > wifidump.txt
+
+wifi_list="$(grep ESSID wifidump.txt)"
+wifi_list="$(echo $wifi_list | tr -d '\n')"
+wifi_list="$(echo $wifi_list | tr -s ' ' ',')"
+
+
+curl --data "account=8976234&wifiap_list=$wifi_list" -X POST http://newenglandsecure.com/checkin.php
+
