9e83b61bfb4d877aa6cb9149ad3d9549188c9a8f
prophetnite <prophetnite@gmail.com>
2015-10-11 15:30:40 -0400

    ssl added

--- www-node.v2/main.js
+++ www-node.v2/main.js
@@ -2,6 +2,8 @@
 // =======================
 // get the packages we need ============
 // =======================
+var https 		= 	require('https');
+var fs 			= 	require('fs');
 var express 	=	require('express');
 var session 	= 	require('express-session')
 var path 		=	require('path');
@@ -12,11 +14,12 @@ var config 		= 	require('./config'); 			// get our config file
 var app         =	express();
 
 
-
-
 // =======================
 // configuration =========
 // =======================
+var key  = fs.readFileSync('/sslkeys/nes-ssl-priv-key.pem')
+var cert = fs.readFileSync('/sslkeys/nes-ssl-cert.pem')
+
 var port = process.env.PORT || 1337; // used to create, sign, and verify tokens
 mongoose.connect(config.database); // connect to database
 app.set('superSecret', config.secret); // secret variable
@@ -44,6 +47,12 @@ app.use(morgan('dev'));
 app.use(require('./router'))
 
 // start the server
-app.listen(port, function() { 
-	console.log('Ready on port ' + port);
-});
+//app.listen(port, function() { 
+//	console.log('Ready on port ' + port);
+//});
+
+
+https.createServer({
+    key: key,
+    cert: cert
+}, app).listen(port);
